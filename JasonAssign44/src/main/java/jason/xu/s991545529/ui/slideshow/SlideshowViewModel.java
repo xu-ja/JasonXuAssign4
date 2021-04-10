fcfaaebb1e6e01607f0a7af61bd8966206705318
xuja <xuja@sheridancollege.ca>
2021-04-10 13:35:15 -0400

    Initial commit

---
+++ JasonAssign44/src/main/java/jason/xu/s991545529/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package jason.xu.s991545529.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
