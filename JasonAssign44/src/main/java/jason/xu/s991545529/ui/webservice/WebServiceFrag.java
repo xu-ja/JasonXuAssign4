0c97d1576cdc43745971346cec43182678129908
xuja <xuja@sheridancollege.ca>
2021-04-11 10:22:12 -0400

    Added WebServiceFrag

--- JasonAssign44/src/main/java/jason/xu/s991545529/ui/webservice/WebServiceFrag.java
+++ JasonAssign44/src/main/java/jason/xu/s991545529/ui/webservice/WebServiceFrag.java
@@ -1,4 +1,4 @@
-package jason.xu.s991545529.ui.slideshow;
+package jason.xu.s991545529.ui.webservice;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,22 +14,14 @@ import androidx.lifecycle.ViewModelProvider;
 
 import jason.xu.s991545529.R;
 
-public class SlideshowFragment extends Fragment {
+public class WebServiceFrag extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_webservice, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
