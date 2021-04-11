34d271439cabe805c32635ca5e06df8afc31c56c
xuja <xuja@sheridancollege.ca>
2021-04-10 22:16:51 -0400

    Added Spinner feature to DownloadFrag

---
+++ JasonAssign44/src/main/java/jason/xu/s991545529/ui/download/CustomAdapter.java
@@ -0,0 +1,63 @@
+package jason.xu.s991545529.ui.download;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import jason.xu.s991545529.R;
+
+public class CustomAdapter extends ArrayAdapter<String> {
+
+    String[] spinnerTitles;
+    int[] spinnerImages;
+    Context context;
+
+    public CustomAdapter(@NonNull Context con, String[] titles, int[] images) {
+        super(con, R.layout.spinner_row);
+        spinnerTitles = titles;
+        spinnerImages = images;
+        context = con;
+    }
+
+    @Override
+    public int getCount() {
+        return spinnerTitles.length;
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder mViewHolder = new ViewHolder();
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater) context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.spinner_row, parent, false);
+            mViewHolder.spinnerImageView = (ImageView) convertView.findViewById(R.id.jasonSpinnerImageView);
+            mViewHolder.spinnerTextView = (TextView) convertView.findViewById(R.id.jasonSpinnerTextView);
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+        mViewHolder.spinnerImageView.setImageResource(spinnerImages[position]);
+        mViewHolder.spinnerTextView.setText(spinnerTitles[position]);
+
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return getView(position, convertView, parent);
+    }
+
+    private static class ViewHolder {
+        ImageView spinnerImageView;
+        TextView spinnerTextView;
+    }
+}
--- JasonAssign44/src/main/java/jason/xu/s991545529/ui/download/DownloadFrag.java
+++ JasonAssign44/src/main/java/jason/xu/s991545529/ui/download/DownloadFrag.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,12 +21,31 @@ import jason.xu.s991545529.R;
 public class DownloadFrag extends Fragment {
 
     View root;
+    Spinner spinner;
+    String spinnerSelection;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         root = inflater.inflate(R.layout.fragment_download, container, false);
 
+        String[] spinnerTitles;
+        int[] spinnerImages;
+
+        spinnerTitles = getResources().getStringArray(R.array.spinner_items);
+        spinnerImages = new int[]{R.drawable.flower, R.drawable.nature, R.drawable.sky};
+
+        CustomAdapter customAdapter = new CustomAdapter(root.getContext(), spinnerTitles, spinnerImages);
+        spinner.setAdapter(customAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView adapterView, View view, int i, long l) {
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView adapterView){}
+        });
 
         return root;
     }
