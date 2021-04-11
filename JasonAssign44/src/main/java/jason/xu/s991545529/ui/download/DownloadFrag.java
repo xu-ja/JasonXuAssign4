5ad39455cfc2feebae5f513a532560846476fb3e
xuja <xuja@sheridancollege.ca>
2021-04-10 23:45:25 -0400

    removed some lines of code used for debugging purposes

--- JasonAssign44/src/main/java/jason/xu/s991545529/ui/download/DownloadFrag.java
+++ JasonAssign44/src/main/java/jason/xu/s991545529/ui/download/DownloadFrag.java
@@ -92,20 +92,6 @@ public class DownloadFrag extends Fragment {
         return root;
     }
 
-/*    public Bitmap getBitmapFromURL(String src) {
-        try {
-            java.net.URL url = new java.net.URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }*/
     private class DownloadTask extends AsyncTask<URL,Void,Bitmap>{
 
         protected void onPreExecute(){
